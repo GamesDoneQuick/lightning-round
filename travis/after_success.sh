fa508ac77e485a1d3ed4585195509beb1c83da7e
Alex Van Camp <vancamp.alex@gmail.com>
2017-12-28 15:38:35 -0600

    build: don't allow scripts to continue after an error

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sentry-cli releases finalize "$TRAVIS_COMMIT"
-firebase deploy --token "$FIREBASE_TOKEN"
-firebase database:set --token "$FIREBASE_TOKEN" --data \"$TRAVIS_COMMIT\" --confirm /deploy_commit_hash
-sentry-cli releases deploys "$TRAVIS_COMMIT" new -e "production"
+sentry-cli releases finalize "$TRAVIS_COMMIT" || exit 1
+firebase deploy --token "$FIREBASE_TOKEN" || exit 1
+firebase database:set --token "$FIREBASE_TOKEN" --data \"$TRAVIS_COMMIT\" --confirm /deploy_commit_hash || exit 1
+sentry-cli releases deploys "$TRAVIS_COMMIT" new -e "production" || exit 1
