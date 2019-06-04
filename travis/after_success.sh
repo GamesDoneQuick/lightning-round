cf61d3d4277ef29564457170657e1cad73c31948
Alex Van Camp <vancamp.alex@gmail.com>
2019-06-04 15:49:15 -0500

    build: deploy to prod and dev

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 sentry-cli releases finalize "$TRAVIS_COMMIT" || exit 1
-firebase deploy --token "$FIREBASE_TOKEN" || exit 1
+firebase deploy -P development --token "$FIREBASE_TOKEN" || exit 1
+firebase deploy -P production --token "$FIREBASE_TOKEN" || exit 1
 firebase database:set --token "$FIREBASE_TOKEN" --data \"$TRAVIS_COMMIT\" --confirm /deploy_commit_hash || exit 1
 sentry-cli releases deploys "$TRAVIS_COMMIT" new -e "production" || exit 1
