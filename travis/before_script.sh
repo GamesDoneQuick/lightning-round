262e54f85347cfb672ddf6f77cc1e0915f02b521
Alex Van Camp <vancamp.alex@gmail.com>
2019-06-04 15:49:01 -0500

    build: don't lowercase org name

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sentry-cli releases set-commits "$TRAVIS_COMMIT" --commit "${TRAVIS_REPO_SLUG,,}@$TRAVIS_COMMIT" || exit 1
+sentry-cli releases set-commits "$TRAVIS_COMMIT" --commit "$TRAVIS_REPO_SLUG@$TRAVIS_COMMIT" || exit 1
 sentry-cli releases new "$TRAVIS_COMMIT" || exit 1
